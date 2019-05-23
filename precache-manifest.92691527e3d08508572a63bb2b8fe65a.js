ed38c14e82c09e351a5cc81d45d207def6b2c25b
Gustavo <villenataipelg@gmail.com>
2019-05-23 15:12:38 -0500

    Proyecto v1.01

---
+++ precache-manifest.92691527e3d08508572a63bb2b8fe65a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "566d2a084fba8498ff8a15c9e968bd6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7744996e2c0f6ce285e",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "92625e8f421b478da1a1",
+    "url": "/static/js/2.5ca6e9a6.chunk.js"
+  },
+  {
+    "revision": "f7744996e2c0f6ce285e",
+    "url": "/static/js/main.144d9a04.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
